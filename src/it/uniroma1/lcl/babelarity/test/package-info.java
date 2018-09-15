f9ec81b786e7676339043fef1f4f0fb587d92515
Eduardo <eduardo.rinaldi1998@gmail.com>
2018-09-15 09:00:58 +0200

    Removed test package

--- src/it/uniroma1/lcl/babelarity/test/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Questo package contiene le classi utili a testare la
- * correttezza delle implementazioni di {@code  MiniBabelNet} e le sue componenti.
- */
-package it.uniroma1.lcl.babelarity.test;
